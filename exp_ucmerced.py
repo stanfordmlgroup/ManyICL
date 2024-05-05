a81c7387f61db2e01d643e8367a33561006e114e
Yixing <108294040+jyx-su@users.noreply.github.com>
2024-05-05 11:58:29 -0700

    Update exp_ucmerced.py

--- exp_ucmerced.py
+++ exp_ucmerced.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 from prompt import work
 
-SAVE_FOLDER = 'dataset/UCMerced_21'
+IMAGE_FOLDER = 'dataset/UCMerced_21'
 demo_df = pd.read_pickle('dataset/UCMerced_demo_21.pkl')
 test_df = pd.read_pickle('dataset/UCMerced_test_21.pkl')
 classes = list(demo_df.columns)
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     num_shot_per_class = args.num_shot_per_class
     num_qns_per_round = args.num_qns_per_round
     
-    work(model, num_shot_per_class, location, num_qns_per_round, test_df, demo_df, classes, class_desp, SAVE_FOLDER, file_suffix, exclude, dataset_name)
+    work(model, num_shot_per_class, location, num_qns_per_round, test_df, demo_df, classes, class_desp, IMAGE_FOLDER, file_suffix, exclude, dataset_name)
